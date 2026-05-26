b47003c2d36baa19c9e6b4df131e1501c38c09d5
plipph <keenan@plip.ph>
2026-05-27 04:05:44 +0800

    analytics sidebar v9

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,9 @@
 
 import { ReactNode } from "react";
 
-import { DashboardShell } from "@/components/dashboard/dashboard-shell";
+import { DashboardSidebar } from "./components/dashboard-sidebar";
+
+import { DashboardMobileNav } from "./components/dashboard-mobile-nav";
 
 type DashboardLayoutProps = {
   children: ReactNode;
@@ -11,9 +13,23 @@ export default function DashboardLayout({
   children,
 }: DashboardLayoutProps) {
   return (
-    <DashboardShell>
-      {children}
-    </DashboardShell>
+    <div className="flex min-h-screen bg-[#F7F1E8]">
+      {/* Desktop Sidebar */}
+      <div className="hidden lg:block">
+        <DashboardSidebar />
+      </div>
+
+      {/* Main Layout */}
+      <div className="min-w-0 flex-1">
+        {/* Mobile Nav */}
+        <DashboardMobileNav />
+
+        {/* Page Content */}
+        <main className="min-h-screen">
+          {children}
+        </main>
+      </div>
+    </div>
   );
 }
 
